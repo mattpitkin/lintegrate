0e9c460d30aa0db4240097fb8bbbade7ee149770
Matthew Pitkin <matthew.pitkin@ligo.org>
2017-06-02 23:21:23 +0100

    qkrules.c: minor bug fix

--- qkrules.c
+++ qkrules.c
@@ -440,6 +440,6 @@ void lintegration_qk61 (const gsl_function * f, double a, double b,
                         double *result, double *abserr,
                         double *resabs, double *resasc){
   double fv1[31], fv2[31];
-  gsl_integration_qk (31, xgk61, wg61, wgk61, fv1, fv2, f, a, b, result, abserr, resabs, resasc);
+  lintegration_qk (31, xgk61, wg61, wgk61, fv1, fv2, f, a, b, result, abserr, resabs, resasc);
 }
 
