3e0005642f530a7fe4e6e53dd76fb01871753a6b
Matthew Pitkin <matthew.pitkin@ligo.org>
2017-06-05 15:04:07 +0100

    example.c: update to also include using lintegrate_qng

--- example/example.c
+++ example/example.c
@@ -36,8 +36,15 @@ int main( int argv, char **argc ){
   gsl_function F;
   struct intparams params;
   gsl_integration_workspace *w = gsl_integration_workspace_alloc (100);
-  double lanswer = 0., answer = 0.;
+  double qaganswer = 0., qnganswer = 0., answer = 0.;
   double abserr = 0.;
+  size_t neval = 0;
+
+  double minlim = -6.; /* minimum for integration range */
+  double maxlim = 6.;  /* maximum for integration range */
+
+  double abstol = 1e-10; /* absolute tolerance */
+  double reltol = 1e-10; /* relative tolerance */
 
   params.mu = 0.;
   params.sig = 1.;
@@ -45,16 +52,22 @@ int main( int argv, char **argc ){
   F.function = &lintegrand;
   F.params = &params;
 
-  /* integrate log of function */
-  lintegration_qag(&F, -6., 6., 1e-10, 1e-10, 100, GSL_INTEG_GAUSS31, w, &lanswer, &abserr);
+  /* integrate log of function using QAG */
+  lintegration_qag(&F, minlim, maxlim, abstol, reltol, 100, GSL_INTEG_GAUSS31, w, &qaganswer, &abserr);
 
-  /* integrate function */
+  /* integrate log of function using QNG */
+  lintegration_qng(&F, minlim, maxlim, abstol, reltol, &qnganswer, &abserr, &neval);
+
+  /* integrate function using GSL QAG */
   F.function = &integrand;
-  gsl_integration_qag(&F, -6., 6., 1e-10, 1e-10, 100, GSL_INTEG_GAUSS31, w, &answer, &abserr);
+  gsl_integration_qag(&F, minlim, maxlim, abstol, reltol, 100, GSL_INTEG_GAUSS31, w, &answer, &abserr);
 
   gsl_integration_workspace_free(w);
 
-  fprintf(stdout, "Answer 1 = %.8lf, Answer 2 = %.8lf, exact answer = %.8lf\n", lanswer, log(answer), log(sqrt(2.*M_PI)));
+  fprintf(stdout, "Answer \"lintegrate QAG\" = %.8lf\n", qaganswer);
+  fprintf(stdout, "Answer \"lintegrate QNG\" = %.8lf\n", qnganswer);
+  fprintf(stdout, "Answer \"gsl_integrate_qag\" = %.8lf\n", log(answer));
+  fprintf(stdout, "Analytical answer = %.8lf\n", log(sqrt(2.*M_PI)));
 
   return 0;
 }
