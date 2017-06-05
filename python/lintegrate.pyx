5aaa538032c2594d43c7e11fe398a293c72afb4c
Matthew Pitkin <matthew.pitkin@ligo.org>
2017-06-05 23:20:40 +0100

    lintegrate.pyx: remove attempts to add integration of lintegrate_qag function  - need to do something more complicated to try and pass a python function    through to the lintegrate function (maybe like in    https://github.com/saullocastro/cubature/blob/master/cubature/_cubature.pyx)  - maybe add this in the future

--- python/lintegrate.pyx
+++ python/lintegrate.pyx
@@ -12,23 +12,6 @@ from scipy.misc import logsumexp
 
 from libc.math cimport exp, sqrt, log
 
-cdef extern from "gsl/gsl_math.h":
-    ctypedef struct gsl_function:
-        double (* function) (double x, void * params)
-        void * params
-
-cdef extern from "gsl/gsl_integration.h":
-    void  gsl_integration_cquad_workspace_free (gsl_integration_cquad_workspace * w)
-    ctypedef struct gsl_integration_workspace
-    gsl_integration_workspace * gsl_integration_workspace_alloc (size_t n)
-
-
-# import lintegration functions
-cdef extern from "lintegrate.h":
-    int lintegration_qag(const gsl_function *f, double a, double b, double epsabs, double epsrel, size_t limit, int key, gsl_integration_workspace * workspace, double * result, double * abserr)
-    int lintegration_qng (const gsl_function *f, double a, double b, double epsabs, double epsrel, double * result, double * abserr, size_t * neval)
-
-
 """
 Simple function to perform trapezium rule integration of a function when given its natural log
 """
@@ -80,6 +63,8 @@ def logtrapz(f, x, args=()):
             assert len(x) > 1, "Function must be evaluated at more than one point"
 
             try:
+                if not isinstance(args, tuple):
+                    args = (args,)
                 vs = f(np.array(x), *args) # make sure x is an array when passed to function
             except:
                 raise Exception('Error... could not evaluate function "f"')
@@ -95,54 +80,4 @@ def logtrapz(f, x, args=()):
             raise Exception('Error... "x" must be a numpy array or list')
     else:
         raise Exception('Error... "f" must be a numpy array, list, or callable function')
-
-
-ctypedef struct lparams:
-    void *function # python function object
-    void *args     # arguments
-
-ctypedef lparams * lparams_ptr
-
-
-cpdef functionwrapper(double x, void *p):
-    cdef lparams * params = <lparams_ptr>p
-
-    func = <object>p.function
-    args = <object>p.args
-
-    try:
-        func(x, *args)
-    except:
-       raise Exception('Error... could not call function')
-
-
-cpdef qag(func, double a, double b, args=(), epsabs=1.49e-8, epsrel=1.49e-8, limit=50, key=3):
-    """
-    Use `lintegrate_qag`
-    """
-
-    cdef gsl_function F
-    cdef lparams params
-
-    params.function = <void*>func
-    params.args = <void*>args
-
-    F.function = &functionwrapper
-    F.params = params
-
-    cdef gsl_integration_workspace *w = gsl_integration_workspace_alloc(limit)
-
-    cdef double result = 0.
-    cdef double err = 0.
-
-    cdef int intkey = key # default is GSL_INTEG_GAUSS31
-    cdef int suc = 0
-
-    gsl_integration_workspace_free(w)
-
-    suc = lintegrate_qag(&F, a, b, epsabs, epsrel, limit, intkey, &w, &result, &err)
-
-    assert suc == 0, 'Error... integration not successful'
-
-    return result, err
 
