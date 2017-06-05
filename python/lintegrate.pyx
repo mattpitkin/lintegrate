c7fd0ca5daeec072f2293e83b2f6b05c0e2fd011
Matthew Pitkin <matthew.pitkin@ligo.org>
2017-06-05 16:41:23 +0100

    lintegrate.pyx: start adding python function versions

---
+++ python/lintegrate.pyx
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+
+import cython
+cimport cython
+
+import numpy as np
+cimport numpy as np
+
+from numpy.math cimport LOGE2
+
+from scipy.misc import logsumexp
+
+from libc.math cimport exp, sqrt, log
+
+"""
+Simple function to perform trapezium rule integration of a function when given its natural log
+"""
+def logtrapz(f, x, args=()):
+    """
+    Given the natural logarithm "f" of some function "g" (i.e., f = log(g)), compute the natural logarithm of the
+    integral of that function using the trapezium rule. 
+
+    Parameters
+    ----------
+    f : :class:`numpy.ndarray`, list, or function handle
+        A set of values of the logarithm of a function evaluated at points given by `x`, or a
+       function handle of the log function to be evaluated.
+    x : :class:`numpy.ndarray`, list, or float
+        An array of values at which `f` has been evaluated, or is to be evaluated at. Or, provided
+        `f` is also an array, a single value giving the spacing between evalation points (if the
+        function has been evaluated on an evenly spaced grid).
+    args : tuple
+        A tuple of any additional parameters required by the function `f`
+  
+    Returns
+    -------
+    I : double
+        The natural logarithm of the intergal of the function
+
+    """
+
+    if isinstance(f, np.ndarray) or isinstance(f, list):
+        if isinstance(x, np.ndarray) or isinstance(x, list):
+            # check arrays are the same length
+            assert len(f) == len(x) and len(x) > 1, "Function and function evaluation points are not the same length"
+
+            # make sure x values are in ascending order (keeping f values associated to their x evaluation points)
+            zp = np.array(sorted(zip(x, f)))
+
+            deltas = np.log(np.diff(zp[:,0])) # get the differences between evaluation points
+
+            # perform trapezium rule
+            return -LOGE2 + logsumexp([logsumexp(zp[:-1,1]+deltas), logsumexp(zp[1:,1]+deltas)])
+        elif isinstance(x, float):
+            assert x > 0., "Evaluation spacings must be positive"
+
+            # perform trapezium rule
+            return np.log(x/2.) + logsumexp([logsumexp(f[:-1]), logsumexp(f[1:])])
+        else:
+            raise Exception('Error... value of "x" must be a numpy array or a float')
+    elif callable(f): # f is a function
+        if isinstance(x, np.ndarray) or isinstance(x, list):
+            assert len(x) > 1, "Function must be evaluated at more than one point"
+
+            try:
+                vs = f(np.array(x), *args) # make sure x is an array when passed to function
+            except:
+                raise Exception('Error... could not evaluate function "f"')
+
+            # make sure x values are in ascending order (keeping f values associated to their x evaluation points)
+            zp = np.array(sorted(zip(x, vs)))
+
+            deltas = np.log(np.diff(zp[:,0])) # get the differences between evaluation points
+
+            # perform trapezium rule
+            return -LOGE2 + logsumexp([logsumexp(zp[:-1,1]+deltas), logsumexp(zp[1:,1]+deltas)])
+        else:
+            raise Exception('Error... "x" must be a numpy array or list')
+    else:
+        raise Exception('Error... "f" must be a numpy array, list, or callable function')
+
---
+++ python/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+from setuptools import Extension
+
+import os, sys, numpy
+
+ext_modules = [ Extension("lintegrate", 
+                          sources =[ "lintegrate.pyx", "../src/lintegrate_qag.c", "../src/lintegrate_qng.c"], 
+                          include_dirs=[numpy.get_include(), '.', os.popen('gsl-config --cflags').read()[2:-1], '../src'],  
+                          library_dirs=['.', os.popen('gsl-config --libs').read().split()[0][2:]], 
+                          libraries=['gsl', 'gslcblas'], extra_compile_args=['-O3']) ]
+
+setup(
+  name = 'lintegrate',
+  version = '0.0.1',
+  url = 'https://github.com/mattpitkin/lintegrate',
+  description = 'Python functions implementing numerical integration of functions in log-space.',
+  author = 'Matthew Pitkin',
+  author_email = 'matthew.pitkin@glasgow.ac.uk',
+  packages = find_packages(),
+  setup_requires = ['numpy'],
+  install_requires = ['numpy', 'scipy', 'cython'],
+  ext_modules = ext_modules,
+  classifiers=[
+      'Intended Audience :: Science/Research',
+      'License :: OSI Approved :: GNU General Public License (GPL)',
+      'Operating System :: POSIX :: Linux',
+      'Programming Language :: Python',
+      'Programming Language :: C',
+      'Natural Language :: English'
+      ]
+)
+
