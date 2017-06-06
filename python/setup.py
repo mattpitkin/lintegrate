cdcc79dee816dc3c0598a53620e260262cda3d23
Matthew Pitkin <matthew.pitkin@ligo.org>
2017-06-06 16:42:07 +0100

    setup.py: get version correctly

--- python/setup.py
+++ python/setup.py
@@ -5,7 +5,7 @@ from setuptools import Extension
 
 from lintegrate import __version__
 
-import os, sys, numpy
+import os, sys, numpy, re
 
 ext_modules = [ Extension("lintegrate", 
                           sources =[ "lintegrate.pyx", "../src/lintegrate_qag.c", "../src/lintegrate_qng.c"], 
@@ -13,9 +13,19 @@ ext_modules = [ Extension("lintegrate",
                           library_dirs=['.', os.popen('gsl-config --libs').read().split()[0][2:]], 
                           libraries=['gsl', 'gslcblas'], extra_compile_args=['-O3', '-DHAVE_PYTHON_LINTEGRATE']) ]
 
+# get version string for pyx file (see e.g. https://packaging.python.org/single_source_version/)
+def find_version():
+  fp = open('lintegrate.pyx', 'r')
+  version_file = fp.read()
+  fp.close()
+  version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M)
+  if version_match:
+    return version_match.group(1)
+  raise RuntimeError("Unable to find version string.")
+
 setup(
   name = 'lintegrate',
-  version = __version__,
+  version = find_version(),
   url = 'https://github.com/mattpitkin/lintegrate',
   description = 'Python functions implementing numerical integration of functions in log-space.',
   author = 'Matthew Pitkin',
