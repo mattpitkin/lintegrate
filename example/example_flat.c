bb0904d8cea46bb1fa0b5e4beb48acb1950c6072
Matthew Pitkin <matthew.pitkin@ligo.org>
2017-06-07 15:48:40 +0100

    example_flat.c: an example to test a flat function

---
+++ example/example_flat.c
@@ -0,0 +1,55 @@
+/* example using lintegrate functionality */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <gsl/gsl_math.h>
+#include <gsl/gsl_integration.h>
+
+#include <lintegrate.h>
+
+/* create function for integration */
+double lintegrand(double x, void *params);
+
+double lintegrand(double x, void *params){
+  return 50.;
+}
+
+double integrand(double x, void *params){
+  return exp(50.);
+}
+
+int main( int argv, char **argc ){
+  gsl_function F;
+  gsl_integration_workspace *w = gsl_integration_workspace_alloc (100);
+  double qaganswer = 0., qnganswer = 0., answer = 0.;
+  double abserr = 0.;
+  size_t neval = 0;
+
+  double minlim = -6.; /* minimum for integration range */
+  double maxlim = 6.;  /* maximum for integration range */
+
+  double abstol = 1e-10; /* absolute tolerance */
+  double reltol = 1e-10; /* relative tolerance */
+
+  F.function = &lintegrand;
+
+  /* integrate log of function using QAG */
+  lintegration_qag(&F, minlim, maxlim, abstol, reltol, 100, GSL_INTEG_GAUSS31, w, &qaganswer, &abserr);
+
+  /* integrate log of function using QNG */
+  lintegration_qng(&F, minlim, maxlim, abstol, reltol, &qnganswer, &abserr, &neval);
+
+  /* integrate function using GSL QAG */
+  F.function = &integrand;
+  gsl_integration_qag(&F, minlim, maxlim, abstol, reltol, 100, GSL_INTEG_GAUSS31, w, &answer, &abserr);
+
+  gsl_integration_workspace_free(w);
+
+  fprintf(stdout, "Answer \"lintegrate QAG\" = %.8lf\n", qaganswer);
+  fprintf(stdout, "Answer \"lintegrate QNG\" = %.8lf\n", qnganswer);
+  fprintf(stdout, "Answer \"gsl_integrate_qag\" = %.8lf\n", log(answer));
+  fprintf(stdout, "Analytic answer = %.8lf\n", log(maxlim-minlim) + 10.);
+
+  return 0;
+}
