5788cfb0f7123405aa504181e9fcd31f502c8eab
Matthew Pitkin <matthew.pitkin@ligo.org>
2017-07-04 12:05:44 +0100

    log[add/sub].c: split logaddexp and logsubexp into seperate files

--- src/logsub.c
+++ src/logsub.c
@@ -18,22 +18,18 @@
 #include <gsl/gsl_math.h>
 #include <gsl/gsl_sf_log.h>
 
-/* function to perform log(exp(lna) + exp(lnb)) maintaining numerical precision */
-static double logaddexp(const double x, const double y);
+/* function to perform log(exp(lna) - exp(lnb)) maintaining numerical precision */
+static double logsubexp(const double x, const double y);
 
-/* function to perform log(exp(lna) + exp(lnb)) maintaining numerical precision */
-double logaddexp(const double x, const double y){
+#define LOGDIFF(x, y) ((x) < (y) ? logsubexp(y, x) : logsubexp(x, y))
+
+/* function to perform log(exp(lna) - exp(lnb)) maintaining numerical precision */
+double logsubexp(const double x, const double y){
   double tmp = x - y;
-  if ( x == y || fabs(tmp) < 1e3*GSL_DBL_EPSILON ){ return x + M_LN2; } /* require the x == y to deal with cases when x and y are both -inf */
+  if ( x > y && fabs(tmp) > 1e3*GSL_DBL_EPSILON ){ /* numbers smaller than this can just give numerical noise in the gsl_sf_log_1plusx function */
+    return x + gsl_sf_log_1plusx(-exp(-tmp));
+  }
   else{
-    if ( tmp > 0. ){
-      return x + gsl_sf_log_1plusx(exp(-tmp));
-    }
-    else if ( tmp <= 0. ){
-      return y + gsl_sf_log_1plusx(exp(tmp));
-    }
-    else{
-      return tmp;
-    }
+    return -INFINITY;
   }
 }
