d0866913a895514a3ce701e3220244cc35c8846a
Matthew Pitkin <matthew.pitkin@ligo.org>
2019-05-31 14:21:58 +0100

    Update Cython generated file

--- python/src/lintegrate.pyx
+++ python/src/lintegrate.pyx
@@ -28,7 +28,7 @@ from scipy.misc import logsumexp
 
 from libc.math cimport exp, sqrt, log, log10, isinf, fabs
 
-__version__ = '0.0.10'
+__version__ = '0.0.11'
 
 cdef extern from "gsl/gsl_integration.h":
     void gsl_integration_workspace_free (gsl_integration_workspace * w)
