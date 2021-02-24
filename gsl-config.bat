4ead22a08b35905fe2104d5a11e2451897f8458c
Duncan Macleod <duncan.macleod@ligo.org>
2021-02-24 16:43:54 +0000

    gsl-config.bat: upgrades for windows
    - `LIBRARY_PREFIX` is the variable set by conda-build for `%PREFIX%\Library` on windows
    - we can run `gsl-config` for version, so we don't have to maintain it

--- gsl-config.bat
+++ gsl-config.bat
@@ -1,17 +1,16 @@
 @echo off
 rem simple Windows version of gsl-config for conda installs
-set ROOT=%CONDA_PREFIX%\Library
+set LIBRARY_PREFIX=%CONDA_PREFIX%\Library
 
-set XCFLAGS=-I"%ROOT%\include"
-set XLIBS1=-L"%ROOT%\lib" -lgsl -lgslcblas
-set XLIBS2=-L"%ROOT%\lib" -lgsl
-set XVERSION=2.3
-set XPREFIX=%ROOT%
+set XCFLAGS=-I%LIBRARY_PREFIX%\include
+set XLIBS1=-L%LIBRARY_PREFIX%\lib -lgsl -lgslcblas
+set XLIBS2=-L%LIBRARY_PREFIX%\lib -lgsl
+set XPREFIX=%LIBRARY_PREFIX%
 
 for %%p in (%*) do (
     if x%%p == x--cflags echo %XCFLAGS%
     if x%%p == x--libs echo %XLIBS1%
     if x%%p == x--libs-without-cblas echo %XLIBS2%
-    if x%%p == x--version echo %XVERSION%
-    if x%%p == x--prefix echo %XPREFIX%
+    if x%%p == x--version bash "%LIBRARY_PREFIX%\bin\gsl-config" --version
+    if x%%p == x--prefix echo %LIBRARY_PREFIX%
 )
